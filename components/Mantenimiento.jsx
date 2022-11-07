1a8d48bcceb65a906f9cb32e2304213db2583ee8
raulDeDevWorld <proyection009@gmail.com>
2022-11-07 18:48:18 -0400

    DC

--- components/Mantenimiento.jsx
+++ components/Mantenimiento.jsx
@@ -1,18 +1,21 @@
 import style from '../styles/Mantenimiento.module.css'
 
-export default function Mantenimiento (props) {
+export default function Mantenimiento(props) {
     return (
-        
+
         <div className={style.container}>
-            <video
-              muted
-              autoPlay={"autoplay"}
-              preload="auto"
-              loop
-              className={style.video}>
-              <source src="/video.mp4" type="video/mp4" />
-            </video>
-            <img className={style.img} src="/mantenimiento.png" alt="Mantenimiento" />
+            
+                <video
+                    muted
+                    autoPlay={"autoplay"}
+                    preload="auto"
+                    loop
+                    className={style.video}>
+                    <source src="/video.mp4" type="video/mp4" />
+                </video>
+            
+                <img className={style.img} src="/mantenimiento.png" alt="Mantenimiento" />
+            
         </div>
     )
 }
