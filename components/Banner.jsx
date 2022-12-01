1e70c183d3bcb893698be587a539faf3bdd8f753
raulDeDevWorld <proyection009@gmail.com>
2022-12-01 17:02:03 -0400

    DC

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -11,7 +11,7 @@ import Link from 'next/link'
 export default function Banner({ ruta, carpeta, click }) {
 
     const { userDB, setUserData, setUserSuccess, success, postsIMG, setUserPostsIMG, date, monthAndYear } = useUser()
-    console.log(userDB[ruta]['Posts'])
+    console.log(userDB[ruta])
     const router = useRouter()
     const buttonStyle = {
         width: "30px",
@@ -25,39 +25,39 @@ export default function Banner({ ruta, carpeta, click }) {
     }
 
 
-    function redirect (ruta) {
+    function redirect(ruta) {
         window.open(ruta, '_blank')
     }
 
 
     return (
         <div className={styles.containerFade} >
-            {userDB[ruta] && postsIMG && 
-            
-            
-            <Zoom  transitionDuration={800} duration={2000} scale={1.4}{...properties} indicators={true}>
+            {userDB[ruta] && postsIMG &&
 
-        
-            
-      
-                {
-                    Object.keys(userDB[ruta][carpeta]).map((i, index) =>
-                        <div className="each-slide" key={index}>
-                            
-                            <div>
-                                {
-                                    router.pathname === "/Admin" ?
-                                        <span onClick={()=>click(carpeta)}><img className={styles.sliderIMG} src={postsIMG[`${ruta}/${i}`]} style={{ objectPosition: `center` }} /></span>
-                                        : <span onClick={()=>redirect(userDB[ruta]['BannerBottom'][i].enlace ? userDB[ruta]['BannerBottom'][i].enlace : '#')}><img className={styles.sliderIMG} src={postsIMG[`${ruta}/${i}`]} style={{ objectPosition: `center` }} /></span>
-                                }
-                                <Link href={`https://api.whatsapp.com/send?phone=${userDB[ruta][carpeta][i].whatsapp}&text=Hola%20vi%20su%20anuncion%20en%20el%20PERIODICO%20HOY%20`} legacyBehavior>
-                                    <a target="_blank"><img className={styles.sliderWhatsapp} src={`/SocialMedia/whatsapp.svg`} /></a>
-                                </Link>
 
+                <Zoom transitionDuration={800} duration={2000} scale={1.4}{...properties} indicators={true}>
+
+
+
+
+                    {
+                        Object.keys(userDB[ruta][carpeta]).map((i, index) =>
+                            <div className="each-slide" key={index}>
+
+                                <div>
+                                    {
+                                        router.pathname === "/Admin" ?
+                                            <span onClick={() => click({ key: carpeta, i })}><img className={styles.sliderIMG} src={postsIMG[`${ruta}/${i}`]} style={{ objectPosition: `center` }} /></span>
+                                            : <span onClick={() => redirect(userDB[ruta]['BannerBottom'][i].enlace ? userDB[ruta]['BannerBottom'][i].enlace : '#')}><img className={styles.sliderIMG} src={postsIMG[`${ruta}/${i}`]} style={{ objectPosition: `center` }} /></span>
+                                    }
+                                    <Link href={`https://api.whatsapp.com/send?phone=${userDB[ruta][carpeta][i].whatsapp}&text=Hola%20vi%20su%20anuncion%20en%20el%20PERIODICO%20HOY%20`} legacyBehavior>
+                                        <a target="_blank"><img className={styles.sliderWhatsapp} src={`/SocialMedia/whatsapp.svg`} /></a>
+                                    </Link>
+
+                                </div>
                             </div>
-                        </div>
-                    )}
-          </Zoom>
+                        )}
+                </Zoom>
             }
         </div>)
 }
