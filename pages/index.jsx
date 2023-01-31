5e4e6a272a47b557310d5624d0ba54d0ed44941d
raulDeDevWorld <proyection009@gmail.com>
2023-01-31 17:40:16 -0400

    DC

--- pages/index.jsx
+++ pages/index.jsx
@@ -26,8 +26,7 @@ import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { listAll } from 'firebase/storage'
 
-// const API_KEY = 'AIzaSyBZkk7x_tGRbf-Yg_A7Y9QYcBQe7T9QtWU'
-// const channelID = 'UCXFA6pzESb1NQMsepmhC6Vw'
+
 const YOUTUBE_PLAYLIST_ITEMS_API = 'https://www.googleapis.com/youtube/v3/playlistItems'
 const YOUTUBE_API_KEY = "AIzaSyBZkk7x_tGRbf-Yg_A7Y9QYcBQe7T9QtWU"
 
@@ -38,50 +37,19 @@ function Home() {
   const { userDB, setUserData, monthAndYear, setUserSuccess, success, postsIMG, showImg, showVideo, date, setUserDate } = useUser()
   const router = useRouter()
 
-  const [periodicoPDF, setPeriodicoPDF] = useState(false);
   const [listYT, setListYT] = useState(false);
 
-  function handlerClickEnlace(data) {
-    router.pathname != "/Admin" && window.open(data.href, data.target)
-    router.pathname == "/Admin" && setDataEditor(i)
-    // console.log(data.href, data.target)
-
-  }
-
-
-
-  function whatsappClickHandler() {
-    router.push("https://api.whatsapp.com/send?phone=+59160589090&text=Buenas%20Hoy...")
-  }
-
-  function handlerPDFView(parametro) {
-    parametro && setPeriodicoPDFEffect(true)
-    setPeriodicoPDF(!periodicoPDF)
-  }
-
-
-
-  const periodicoPDFImg = {
-    height: '100%',
-    width: '100%',
-    objectFit: 'contain',
-    objectPosition: 'center',
-    borderRadius: '5px',
-    boxShadow: '0 0 15px black',
-    transition: 'all',
-    transitionDuration: '.3s',
-
-  }
   async function getYB() {
     const res = await fetch(`${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&maxResults=8&playlistId=UULFXFA6pzESb1NQMsepmhC6Vw&key=${YOUTUBE_API_KEY}`)
     const data = await res.json();
     setListYT(data)
   }
 
-
+  function redirectYT () {
+    window.open('https://www.youtube.com/@periodicohoybolivia2201/videos', '_blank')
+  }
 
   useEffect(() => {
-
     getYB()
   }, [])
   console.log(showVideo)
@@ -115,27 +83,37 @@ function Home() {
 
           <div className={styles.gridVideos}>
 
-           
-              {listYT.items.map(({ id, snippet = {} }) => {
-                const { title, thumbnails = {}, resourceId = {} } = snippet;
-                const { medium } = thumbnails;
-                return (
-                  <div    key={id}  className={styles.video}>
-                    
-                    <iframe
-                      width={medium.width} height={medium.height}
-                      src={`https://www.youtube.com/embed/${resourceId.videoId}`}
-                      title="YouTube video player"
-                      frameborder="0"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                      allowfullscreen></iframe>
-    
-                    <h3>{title}</h3>
 
-                  </div>
-                )
-              })}
-         
+            {listYT.items.map(({ id, snippet = {} }) => {
+              const { title, thumbnails = {}, resourceId = {} } = snippet;
+              const { medium } = thumbnails;
+              return (
+                <div key={id} className={styles.boxVideo}>
+
+                  <iframe
+                    className={styles.video}
+                 
+                    src={`https://www.youtube.com/embed/${resourceId.videoId}`}
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen></iframe>
+
+                  {/* <p className={styles.videoDescription}>{title}</p> */}
+
+                </div>
+              )
+            })}
+            <div className={styles.boxVideo} onClick={redirectYT}>
+              <img className={styles.seeMoreYT} 
+              src="/seeMoreYT.jpeg" 
+               title="YouTube video player"
+               frameborder="0"
+               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+               allowfullscreen  />
+              {/* <p className={styles.videoDescription}>Las noticias mas relevantes en <br /> HOY.BO</p> */}
+            </div>
+
           </div>}
 
         {showImg == false && showVideo == false && <>
@@ -158,5 +136,28 @@ function Home() {
 
 export default WithoutAuth(Home)
 
+
+
+
+
+
+
+// function handlerClickEnlace(data) {
+//   router.pathname != "/Admin" && window.open(data.href, data.target)
+//   router.pathname == "/Admin" && setDataEditor(i)
+//   // console.log(data.href, data.target)
+
+// }
+
+
+
+// function whatsappClickHandler() {
+//   router.push("https://api.whatsapp.com/send?phone=+59160589090&text=Buenas%20Hoy...")
+// }
+
+// function handlerPDFView(parametro) {
+//   parametro && setPeriodicoPDFEffect(true)
+//   setPeriodicoPDF(!periodicoPDF)
+// }
 
 
