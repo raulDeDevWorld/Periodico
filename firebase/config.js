238d7d4dbccca1062dbe73cdb99ba92147793eed
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2023-03-08 11:45:26 -0400

    Update config.js
    CC

--- firebase/config.js
+++ firebase/config.js
@@ -1,15 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCz6-81rIlsixQzgB5WsIb6GfvqGpWu4-4",
-  authDomain: "hoy-bo-bd.firebaseapp.com",
-  databaseURL: "https://hoy-bo-bd-default-rtdb.firebaseio.com",
-  projectId: "hoy-bo-bd",
-  storageBucket: "hoy-bo-bd.appspot.com",
-  messagingSenderId: "332334893181",
-  appId: "1:332334893181:web:1295ead91957c8b395c914"
+  apiKey: "AIzaSyD4QAmeTzYQT-QADWRGiRRYPBA4NHxP98c",
+  authDomain: "hoy-bo-db-c485e.firebaseapp.com",
+  databaseURL: "https://hoy-bo-db-c485e-default-rtdb.firebaseio.com",
+  projectId: "hoy-bo-db-c485e",
+  storageBucket: "hoy-bo-db-c485e.appspot.com",
+  messagingSenderId: "585146926149",
+  appId: "1:585146926149:web:9150be28f4c4991a894f2c"
 };
-// 
-
 
 export const app = initializeApp(firebaseConfig)
